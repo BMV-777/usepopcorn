a1527e4916853cf21a20baa4a33d726bfa79e7c6
Borenkov Maksim <borenkovmv@gmail.com>
2024-01-27 22:13:29 +0300

    block4

--- src/probStar.js
+++ src/probStar.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -7,81 +6,55 @@ const containerStyle = {
   gap: "16px",
 };
 
-const starContainerStyle = {
+const styleContainer = {
   display: "flex",
 };
 
-StarRating.propTypes = {
-  maxRating: PropTypes.number,
-  size: PropTypes.number,
-  color: PropTypes.string,
-  messages: PropTypes.array,
-  onRatings: PropTypes.func,
-  className: PropTypes.string,
-  defaultRating: PropTypes.number,
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
 };
 
-export default function StarRating({
-  maxRating = 5,
-  size = 48,
-  color = "#Fcc419",
-  className = "",
-  messages = [],
-  onRatings,
-  defaultRating = 0,
-}) {
-  const [rating, setRating] = useState(defaultRating);
+const ProbStar = ({ maxRating = 5 }) => {
+  const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
 
   function handelRating(rating) {
     setRating(rating);
-    onRatings(rating);
   }
 
-  const textStyle = {
-    lineHeight: "1",
-    margin: "0",
-    color,
-    fontSize: `${size / 1.5}px`,
-  };
-
   return (
-    <div style={containerStyle} className={className}>
-      <div style={starContainerStyle}>
+    <div style={containerStyle}>
+      <div style={styleContainer}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            Rating={() => handelRating(i + 1)}
+            onRating={() => handelRating(i + 1)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
-            color={color}
-            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>
-        {messages.length === maxRating
-          ? messages[tempRating ? tempRating - 1 : rating - 1]
-          : tempRating || rating || ""}
-      </p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
-}
+};
 
-function Star({ Rating, full, onHoverIn, onHoverOut, size, color }) {
-  const starStyle = {
-    height: `${size}px`,
-    width: `${size}px`,
-    display: "block",
-    cursor: "pointer",
-  };
+export default ProbStar;
 
+const starStyle = {
+  width: "48px",
+  height: "48px",
+  cursor: "pointer",
+};
+
+function Star({ onRating, onHoverIn, onHoverOut, full }) {
   return (
     <span
       role="button"
       style={starStyle}
-      onClick={Rating}
+      onClick={onRating}
       onMouseEnter={onHoverIn}
       onMouseLeave={onHoverOut}
     >
@@ -89,8 +62,8 @@ function Star({ Rating, full, onHoverIn, onHoverOut, size, color }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill={color}
-          stroke={color}
+          fill="#000"
+          stroke="#000"
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -99,7 +72,7 @@ function Star({ Rating, full, onHoverIn, onHoverOut, size, color }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke={color}
+          stroke="#000"
         >
           <path
             strokeLinecap="round"
@@ -112,4 +85,3 @@ function Star({ Rating, full, onHoverIn, onHoverOut, size, color }) {
     </span>
   );
 }
-// 10/118
